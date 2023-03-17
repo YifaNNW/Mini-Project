a2f00ffd73f2541e35fe59b69b17a70ef2a6d14d
Cedriccmh <handsomecmh@gmail.com>
2023-03-17 00:18:44 +0000

    add method to select useful data from lobs by tapes timestamp

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def read_file(txt_path):
     with open(txt_path, 'r') as f:
         data = []
@@ -17,10 +20,48 @@ def read_txt_to_list(txt_path):
     return list_data
 
 
-txt_path = "HSBC_Examples/TstUoB_2024-01-02LOBs.txt"
-list_data = read_txt_to_list(txt_path)
+def lob_time(lob):
+    return lob[1]
 
-print(list_data[10:20])
+
+def get_lobs_by_tapes(df_tapes, list_lobs):
+    timestamp = df_tapes['timestamp']
+    list_timestamp = timestamp.tolist()
+    # Select the LOBs that are in the same second as the tape
+    lobs_selected = []
+    idx_lobs = 0
+    for each_timestamp in list_timestamp:
+        while lob_time(list_lobs[idx_lobs]) < each_timestamp:
+            idx_lobs += 1
+        if lob_time(list_lobs[idx_lobs]) == each_timestamp:
+            lobs_selected.append(list_lobs[idx_lobs])
+    return lobs_selected
+
+
+def preprocess_tapes(csv_path):
+    # Read csv file
+    data = pd.read_csv(csv_path)
+    # Remove the first two columns
+    data.drop(data.columns[[0, 1]], axis=1, inplace=True)
+    # Name the columns
+    data.columns = ['timestamp', 'price', 'volume']
+    return data
+
+
+txt_path = "HSBC_Examples/TstUoB_2024-01-02LOBs.txt"
+lobs = read_txt_to_list(txt_path)
+
+csv_path = "HSBC_Examples/TstUoB_2024-01-02tapes.csv"
+tapes = preprocess_tapes(csv_path)
+
+# Get the LOBs that are in the same second as the tape
+lobs_selected = get_lobs_by_tapes(tapes, lobs)
+
+# Save the selected LOBs to a csv file
+df_lobs_selected = pd.DataFrame(lobs_selected)
+df_lobs_selected.to_csv("HSBC_Examples/TstUoB_2024-01-02LOBs_Selected.csv", index=False)
+
+
 
 
 
