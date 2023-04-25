d3fdd7cea35cfd0d5b7bac80e36d4f102d1b0161
Cedriccmh <handsomecmh@gmail.com>
2023-04-25 16:08:23 +0100

    adjust both simulator and lstm to new data set

--- LSTM.py
+++ LSTM.py
@@ -1,30 +1,60 @@
 import numpy as np
 import pandas as pd
+import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras import Input
 from keras.layers import Dense, TimeDistributed
 from keras.utils.np_utils import *
-from sklearn.metrics import classification_report
-
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
 
+# parameters
+batch_size = 128
+epochs = 50
+percentage = 0.7
 
-data = pd.read_csv('2024-01-02LOBs.csv')
-# move 'mid_price' to the last column
-mid_price = data['mid_price']
-data.drop(labels=['mid_price'], axis=1, inplace=True)
-data.insert(len(data.columns), 'mid_price', mid_price)
+# data = pd.read_csv('2024-01-02LOBs.csv')
+# # move 'mid_price' to the last column
+# mid_price = data['mid_price']
+# data.drop(labels=['mid_price'], axis=1, inplace=True)
+# data.insert(len(data.columns), 'mid_price', mid_price)
+#
+# data['next_mid_price'] = data['mid_price'].shift(-1)
+# data.dropna(inplace=True)
+# data['price_movement'] = np.where(data['next_mid_price'] > data['mid_price'], 1, 0)
+# data.drop(['next_mid_price'], axis=1, inplace=True)
 
-data['next_mid_price'] = data['mid_price'].shift(-1)
-data.dropna(inplace=True)
-data['price_movement'] = np.where(data['next_mid_price'] > data['mid_price'], 1, 0)
-data.drop(['next_mid_price'], axis=1, inplace=True)
+# Read the first 70% of the LOBs csv files from the CSV_10 folder
+print("Reading the first 70% of the LOBs csv files from the CSV_10 folder...")
+folder_path = "CSV_10"
+# get csv file from the folder
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+# sort the csv files
+csv_files.sort()
+num_files_to_read = int(len(csv_files) * percentage)
+# select the first 70% of the csv files
+files_to_read = csv_files[:num_files_to_read]
+
+# read the csv files into a list of dataframes
+dataframes = []
+for file in files_to_read:
+    file_path = os.path.join(folder_path, file)
+    df = pd.read_csv(file_path)
+    dataframes.append(df)
+
+# concatenate the dataframes into one dataframe
+combined_df = pd.concat(dataframes, ignore_index=True)
+
+# remove the "rising", "falling" and "stable" columns
+combined_df.drop(['rising', 'falling', 'stable'], axis=1, inplace=True)
+print("combined_df.shape: ", combined_df.shape)
 
 # print(data.shape)
 # print(data.columns)
-data = data.values
+data = combined_df.values
 y = np.expand_dims(data[:, -1], axis=1)
 X = data[:, :-1]
 
@@ -36,7 +66,6 @@ X = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y_binary, test_size=0.3, random_state=42)
 
 batch_size = 128
-
 
 def reshape_to_batches(data, batch_size):
     # Pad the data with zeros to make it fit the batch_size
@@ -52,33 +81,66 @@ y_train_batch = reshape_to_batches(y_train, batch_size)
 X_test_batch = reshape_to_batches(X_test, batch_size)
 y_test_batch = reshape_to_batches(y_test, batch_size)
 
+
+def create_LSTM_model():
+    inputs = Input(shape=(batch_size, X_train.shape[1]))
+    lstm = tf.keras.layers.LSTM(128, return_sequences=True)(inputs)
+    predictions = TimeDistributed(Dense(3, activation='softmax'))(lstm)
+    model = tf.keras.Model(inputs=inputs, outputs=predictions)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
 # Create the model
-inputs = Input(shape=(batch_size, X_train.shape[1]))
-lstm = tf.keras.layers.LSTM(128, return_sequences=True)(inputs)
-
-predictions = TimeDistributed(Dense(2, activation='softmax'))(lstm)
-model = tf.keras.Model(inputs=inputs, outputs=predictions)
-
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+print("Creating the model...")
+model = create_LSTM_model()
 
 # Train the model
-model.fit(X_train_batch, y_train_batch, epochs=10, batch_size=batch_size)
-
-# Evaluate the model
-y_pred = model.predict(X_test_batch)
-print(y_pred)
-print(y_train_batch)
-
-y_pred[np.where(y_pred >= 0.5)] = 1
-y_pred[np.where(y_pred < 0.5)] = 0
-print(classification_report(
-    y_test_batch.reshape(y_test_batch.shape[0] * y_test_batch.shape[1], y_test_batch.shape[2])[:, 1],
-    y_pred.reshape(y_pred.shape[0] * y_pred.shape[1], y_pred.shape[2])[:, 1],
-    target_names=["Down", "Up"],
-    digits=5))
+print("Training the model...")
+model.fit(X_train_batch, y_train_batch, epochs=epochs, batch_size=batch_size)
 
 # Save the model
+print("Saving the model...")
 model.save('LSTM.h5')
+
+# Plot the training loss and accuracy
+print("Plotting the training loss and accuracy...")
+plt.plot(model.history.history['loss'])
+plt.plot(model.history.history['accuracy'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Loss', 'Accuracy'], loc='upper left')
+plt.show()
+
+# Evaluate the model
+print("Evaluating the model...")
+y_pred = model.predict(X_test_batch)
+
+# # y_pred to categorical
+# y_pred = np.argmax(y_pred, axis=2)
+# print(y_pred)
+# # y_test_batch to categorical
+# y_test_batch = np.argmax(y_test_batch, axis=2)
+# print(y_test_batch)
+
+# y_pred to categorical
+y_pred = np.argmax(y_pred, axis=2)
+y_pred = to_categorical(y_pred)
+# y_pred 3D to 2D
+y_pred = np.reshape(y_pred, (y_pred.shape[0] * y_pred.shape[1], y_pred.shape[2]))
+# y_test_batch 3D to 2D
+y_test_batch = np.reshape(y_test_batch, (y_test_batch.shape[0] * y_test_batch.shape[1], y_test_batch.shape[2]))
+
+
+print(classification_report(
+    y_test_batch,
+    y_pred,
+    target_names=["Stable", "Rising", "Falling"],
+    digits=5))
+print("Accuracy: ", accuracy_score(y_test_batch, y_pred))
+
+
 
 
 
--- trading_simulator.py
+++ trading_simulator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 from keras.models import load_model
 from LSTM import reshape_to_batches
 import numpy as np
@@ -6,17 +7,41 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-# Load the selected LOBs
+# Read the last 30% of the LOBs csv files from the CSV_10 folder
 print('Loading the LOBs...')
-data_original = pd.read_csv('2024-01-02LOBs.csv')
+folder_path = "CSV_10"
+# get csv file from the folder
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+# sort the csv files
+csv_files.sort()
+# select the last 30% of the csv files
+files_to_read = csv_files[int(len(csv_files) * 0.7):]
+# read the csv files into a list of dataframes
+dataframes = []
+for file in files_to_read:
+    file_path = os.path.join(folder_path, file)
+    df = pd.read_csv(file_path)
+    dataframes.append(df)
+# concatenate the dataframes into one dataframe
+combined_df = pd.concat(dataframes, ignore_index=True)
+# remove the "rising", "falling" and "stable" columns
+combined_df.drop(['rising', 'falling', 'stable'], axis=1, inplace=True)
+print("combined_df.shape: ", combined_df.shape)
+
+data_original = combined_df
+
+data_original = data_original.iloc[0:2000]
+
 data = data_original.values
+y = np.expand_dims(data[:, -1], axis=1)
 X = data[:, :-1]
 
+# Scale the data
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
 batch_size = 128
-data_batch = reshape_to_batches(data, batch_size)
+data_batch = reshape_to_batches(X, batch_size)
 
 # Load the trained LSTM model
 print("Loading the trained LSTM model...")
@@ -31,33 +56,45 @@ step = 0
 trading_num = 10
 shares_num = 100
 list_buy_sell_index = []
+last_mid_price = 0
 
 for batch in data_batch:
     # Skip the last batch
     if step == steps:
+        # save the last mid_price
+        last_mid_price = data_original['mid_price'].iloc[(step - 1) * batch_size + batch_size - 1]
         break
     # Reshape the batch
     batch = np.expand_dims(batch, axis=0)
 
     y_pred = model.predict(batch)
-
     # Using the last prediction as the signal for trading
-    signal = y_pred[-1][-1][-1]
+    y_pred = y_pred[-1]
+    y_pred = np.argmax(y_pred, axis=1)
+    signal = y_pred[-1]
+    print('signal: ', signal)
     # Get the mid_price of the last row
     mid_price = data_original['mid_price'].iloc[step * batch_size + batch_size - 1]
 
     # If the signal is 1, buy 10 shares
-    if signal >= 0.5:
+    if signal == 1 and capital >= mid_price * trading_num:
         print('Buy 10 shares at ', mid_price)
         shares_num += trading_num
         capital -= mid_price * trading_num
         list_buy_sell_index.append((step * batch_size + batch_size - 1, 'buy', mid_price))
     # If the signal is 0, sell 100 shares
-    elif signal < 0.5 and shares_num >= trading_num:
-        print('Sell 10 shares at ', mid_price)
-        shares_num -= trading_num
-        capital += mid_price * trading_num
+    elif signal == 2 and shares_num >= trading_num:
+        # print('Sell 10 shares at ', mid_price)
+        # shares_num -= trading_num
+        # capital += mid_price * trading_num
+        # list_buy_sell_index.append((step * batch_size + batch_size - 1, 'sell', mid_price))
+
+        # sell all shares
+        print('Sell all shares at ', mid_price)
+        capital += mid_price * shares_num
+        shares_num = 0
         list_buy_sell_index.append((step * batch_size + batch_size - 1, 'sell', mid_price))
+
     else:
         print('Hold at ', mid_price)
 
@@ -66,20 +103,24 @@ for batch in data_batch:
     print('shares_num: ', shares_num)
     step += 1
 
+# Calculate the final capital
+final_capital = capital + last_mid_price * shares_num
+print('final_capital: ', final_capital)
+
 # Plot the first 1000 mid_price and the buy/sell points
 plt.plot(data_original['mid_price'].iloc[:1000], label='mid_price')
 for index, buy_sell, mid_price in list_buy_sell_index:
     if index < 1000:
         if buy_sell == 'buy':
-            plt.scatter(index, mid_price, c="green")
+            plt.scatter(index, mid_price, c="green", zorder=3)
         else:
-            plt.scatter(index, mid_price, c='red')
+            plt.scatter(index, mid_price, c='red', zorder=3)
 
 green_circle = mlines.Line2D([], [], color='green', marker='o', linestyle='None', label='buy')
 red_circle = mlines.Line2D([], [], color='red', marker='o', linestyle='None', label='sell')
+mid_price_line = mlines.Line2D([], [], color='blue', label='mid_price')
 
-plt.legend(handles=[green_circle, red_circle], labels=['buy', 'sell'])
-plt.legend()
+plt.legend(handles=[mid_price_line, green_circle, red_circle])
 
 plt.show()
 
